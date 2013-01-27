8ef71696508bdee2b1df1846b1c60ff4d7fa4222
Thiago Pastor <thiagodiaspastor@gmail.com>
2013-01-27 13:00:22 -0200

    Adding UDP data Replayer
    used for testing

---
+++ UDPDataGenerator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace UDPDataGenerator
+{
+    /// <summary>
+    /// replay a stored file of telemetry data
+    /// </summary>
+    class Program
+    {
+        // Constants
+        private static int PORTNUM = 20777;
+        private static string IP = "127.0.0.1";
+
+        // This is the IP endpoint we are connecting to (i.e. the IP and Port F1 2011 is sending to)
+        private static IPEndPoint remoteIP = new IPEndPoint(IPAddress.Parse(IP), PORTNUM);
+        // This is the IP endpoint for capturing who actually sent the data
+        private static IPEndPoint senderIP = new IPEndPoint(IPAddress.Any, 0);
+        
+        static IPEndPoint groupEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORTNUM);
+
+
+        static void Main(string[] args)
+        {
+
+            List<telemetryPacket> latestData = new List<telemetryPacket>();
+
+            UdpClient udpSocket = new UdpClient();
+            udpSocket.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            FileStream FileStream = File.Open("captura.xml", FileMode.Open);
+            XmlSerializer XmlSerializer = new System.Xml.Serialization.XmlSerializer(latestData.GetType());
+            latestData = (List<telemetryPacket>)XmlSerializer.Deserialize(FileStream);
+            FileStream.Close();
+
+
+            foreach (var item in latestData)
+            {
+                byte[] bts = StructureToByteArray(item);
+                udpSocket.Send(bts, bts.Length, groupEP);
+                Console.WriteLine(item);
+                Thread.Sleep(TimeSpan.FromSeconds(1.0 / 60.0));
+            }            
+        }
+
+        static byte[] StructureToByteArray(object obj)
+        {
+            int len = Marshal.SizeOf(obj);
+            byte[] arr = new byte[len];
+            IntPtr ptr = Marshal.AllocHGlobal(len);
+            Marshal.StructureToPtr(obj, ptr, true);
+            Marshal.Copy(ptr, arr, 0, len);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+    }
+}
---
+++ UDPDataGenerator/telemetryPacket.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace UDPDataGenerator
+{
+        // This is the telemetry packet definition. My thanks to "korrea" for posting
+        // these details on the CM forums
+        [Serializable]
+        public struct telemetryPacket
+        {
+            public static IEnumerable<String> GetFields()
+            {
+                FieldInfo[] infos = typeof(telemetryPacket).GetFields();
+                return infos.Select<FieldInfo, String>((a) => a.Name);
+            }
+
+
+            public float GetFieldValue(String fieldName)
+            {
+                return (float) this.GetType().GetField(fieldName).GetValue(this);   
+            }
+
+
+            public float time;
+            public float lapTime;
+            public float lapDistance;
+            public float distance;
+            public float x;
+            public float y;
+            public float z;
+            public float speed;
+            public float xv;
+            public float yv;
+            public float zv;
+            public float xr;
+            public float yr;
+            public float zr;
+            public float xd;
+            public float yd;
+            public float zd;
+            public float susp_pos_bl;
+            public float susp_pos_br;
+            public float susp_pos_fl;
+            public float susp_pos_fr;
+            public float susp_vel_bl;
+            public float susp_vel_br;
+            public float susp_vel_fl;
+            public float susp_vel_fr;
+            public float wheel_speed_bl;
+            public float wheel_speed_br;
+            public float wheel_speed_fl;
+            public float wheel_speed_fr;
+            public float throttle;
+            public float steer;
+            public float brake;
+            public float clutch;
+            public float gear;
+            public float gforce_lat;
+            public float gforce_lon;
+            public float lap;
+            public float engineRate;
+
+            public override string ToString()
+            {
+                return "Lap: " + lap + " Speed: " + speed + " Time: " + time;
+            }
+        }    
+}
